457985b5aa4a98d005cf21f668409386c8c60643
leon <1092621445@qq.com>
2019-05-18 17:49:55 +0800

    添加feign测试

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/xzp/DeptCustomer80_App.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/xzp/DeptCustomer80_App.java
@@ -3,10 +3,15 @@ package com.xzp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import com.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DeptCustomer80_App {
+//在启动微服务的时候添加如下配置，可以加载自定义Ribbon的配置类（可实现自定义负载均衡）
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration=MySelfRule.class)
+public class DeptCustomer80_App { 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/xzp/cfgbean/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/xzp/cfgbean/ConfigBean.java
@@ -5,13 +5,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+
+// @Configuration用于ConfigBean类上时，相当于 applicationContext.xml文件
 @Configuration
 public class ConfigBean {
 	
 	@Bean
-	@LoadBalanced  //负载均衡
+	@LoadBalanced  //负载均衡（默认轮询）
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
+	}
+	
+	//重写IRule接口，替代默认轮询
+	@Bean
+	public IRule myRule(){
+		return new RandomRule();					//随机
+		//return new RoundRobinRule();				//轮询
+		
+		//先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，还有并发的连接数量超过阈值的服务，然后对剩余的服务列表按照轮询策略进行访问
+		//return new AvailabilityFilteringRule();
+		
+		//根据平均响应时间计算所有服务的权重，越快响应的服务权重越大，刚启动时如果统计信息不足，则使用RoundRobinRule策略，信息足够时切换
+		//return new WeightedResponseTimeRule();	
+		
+		//return new RetryRule();	
+		
+		//先过滤掉由于多次访问故障而处于断路器跳闸状态的服务，然后选择一个并发量最小的服务
+		//return new BestAvailableRule();	
+		
+		//默认规则，复合判断server所在区域的性能和server的可用性选择服务器
+		//return new ZoneAvoidanceRule();
 	}
 	
 }
